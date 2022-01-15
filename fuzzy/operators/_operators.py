fa1f98227d97c1a11bc428a70cf2f7011938a077
Artur Kot <KotTomaszArtur@gmail.com>
2022-01-15 01:45:02 +0100

    Added S-norm, T-norm and Strong Negation operators.

--- fuzzy/operators/_operators.py
+++ fuzzy/operators/_operators.py
@@ -44,3 +44,68 @@ class FuzzyOperator(ABC):
         :param value: value to pass through FuzzyMembershipFunctions
         :return: result of pipeline
         """
+
+class TNorm(FuzzyOperator):
+    """
+    Intersection of fuzzy sets.
+
+    Could be read as "AND" operator.
+    """
+
+    def __call__(self, value: float) -> float:
+        results = []
+        for ff in self.functions:
+            res = ff(value)
+            if res == 0:
+                return 0.
+        return min(results)
+
+
+class SNorm(FuzzyOperator):
+    """
+    Union of fuzzy sets.
+
+    Could be read as "OR" operator.
+    """
+
+    def __call__(self, value: float) -> float:
+        """
+        Pass through fuzzy function and negate option.
+
+        :param value: value to calculate negation on given function
+        :return: negated degree of membership
+        """
+        results = []
+        for ff in self.functions:
+            res = ff(value)
+            if res == 1:
+                return 1.
+        return max(results)
+
+
+class StrongNegation(FuzzyOperator):
+    """
+    Negates scale of membership degree.
+
+    Could be read as "NOT" operator.
+    """
+    def __init__(
+            self,
+            function: Operatable
+    ) -> None:
+        """
+        Create negation for given Operatable object.
+
+        :param function: can be either FuzzyMembershipFunction or
+          FuzzyOperator
+        """
+        super().__init__(function)
+
+    def __call__(self, value: float) -> float:
+        """
+        Pass through fuzzy function and negate option.
+
+        :param value: value to calculate negation on given function
+        :return: negated degree of membership
+        """
+        return 1 - self.functions[0](value)
